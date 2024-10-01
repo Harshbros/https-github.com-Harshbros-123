b856250fc3cba3e44957be29d7721cf5e7659ac0
Harsh <harshitsinghkohli@gmail.com>
2024-10-01 18:27:41 -0400

    Initial Commit

---
+++ team11/team11App.swift
@@ -0,0 +1,17 @@
+//
+//  team11App.swift
+//  team11
+//
+//  Created by Harshit Singh Kohli on 2024-10-01.
+//
+
+import SwiftUI
+
+@main
+struct team11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ team11Tests/team11Tests.swift
@@ -0,0 +1,17 @@
+//
+//  team11Tests.swift
+//  team11Tests
+//
+//  Created by Harshit Singh Kohli on 2024-10-01.
+//
+
+import Testing
+@testable import team11
+
+struct team11Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
